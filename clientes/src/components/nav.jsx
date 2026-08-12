f1c139d5f255901613f535b903262037ca246f55
carri <hamthiago08@gmail.com>
2026-08-11 22:09:54 -0300

    se dan distintos colores a las tarjetas del nav

--- clientes/src/components/nav.jsx
+++ clientes/src/components/nav.jsx
@@ -5,22 +5,22 @@ function Nav() {
     return (
         <>
             <div className='tarjetas'>
-                <div className='tarjeta'>
+                <div className='tarjeta tcli'>
                     <h3 className='tt'>Clientes</h3>
                     <p className='texto'>Pagina de administacion<br />de los clientes</p>
                     <button className='boton'>Ir a clientes</button>
 
                 </div>
-                <div className='tarjeta'>
+                <div className='tarjeta tpro'>
                     <h3 className='tt'>Productos</h3>
                     <p className='texto'>Pagina de administacion<br /> de los productos</p>
-                    <button className='boton'>Ir a productos</button>
+                    <button className='boton bpro'>Ir a productos</button>
 
                 </div>
-                <div className='tarjeta'>
+                <div className='tarjeta tven'>
                     <h3 className='tt'>Ventas</h3>
                     <p className='texto'>Pagina de administacion<br /> de las ventas</p>
-                    <button className='boton'>Ir a ventas</button>
+                    <button className='boton bven'>Ir a ventas</button>
 
                 </div>
 
