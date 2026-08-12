ffd43ad541f82af82f6b785b29c931b49d6ea32b
carri <hamthiago08@gmail.com>
2026-08-11 21:52:09 -0300

    se universalisa el nav

--- clientes/src/components/nav.jsx
+++ clientes/src/components/nav.jsx
@@ -5,22 +5,22 @@ function Nav() {
     return (
         <>
             <div className='tarjetas'>
-                <div className='tarjetac'>
-                    <h3 className='ttc'>Clientes</h3>
+                <div className='tarjeta'>
+                    <h3 className='tt'>Clientes</h3>
                     <p className='texto'>Pagina de administacion<br />de los clientes</p>
-                    <button className='botonc'>Ir a clientes</button>
+                    <button className='boton'>Ir a clientes</button>
 
                 </div>
-                <div className='tarjetap'>
-                    <h3 className='ttp'>Productos</h3>
-                    <p className='textop'>Pagina de administacion<br /> de los productos</p>
-                    <button className='botop'>Ir a productos</button>
+                <div className='tarjeta'>
+                    <h3 className='tt'>Productos</h3>
+                    <p className='texto'>Pagina de administacion<br /> de los productos</p>
+                    <button className='boton'>Ir a productos</button>
 
                 </div>
-                <div className='tarjetav'>
-                    <h3 className='ttv'>Ventas</h3>
-                    <p className='textov'>Pagina de administacion<br /> de las ventas</p>
-                    <button className='botov'>Ir a ventas</button>
+                <div className='tarjeta'>
+                    <h3 className='tt'>Ventas</h3>
+                    <p className='texto'>Pagina de administacion<br /> de las ventas</p>
+                    <button className='boton'>Ir a ventas</button>
 
                 </div>
 
